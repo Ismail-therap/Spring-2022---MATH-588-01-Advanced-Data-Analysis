10a250385778bbe7222ed0eda4c44869ce8d3224
Md. Ismail Hossain <statistician71@gmail.com>
2022-02-23 16:50:00 -0700

    HW4

---
+++ HW4/R_script.R
@@ -0,0 +1,25 @@
+library(Sleuth3)
+bb = case1102
+sapply(bb, class) # Simplify future typeing by changing names to lower case:
+names(bb) = casefold(names(bb)) 
+names(bb)
+
+# Make new variables 
+bb$logBLratio = log(bb$brain/bb$liver)
+bb$logTime = log(bb$time)
+
+# a(1)
+library(dplyr)
+with(bb, table(sex)) %>% prop.table()
+
+
+# a(2)
+with(bb, table(treatment,days)) %>% prop.table()
+
+# b
+
+# Plot key variables
+with(bb, plot(logBLratio ~ logTime, pch=as.numeric(bb$treat), xlab="Log Time"))
+with(bb, table(bb$treat,as.numeric(bb$treat)))
+legend("topleft", legend=c("Barrier Disruption","Saline Control"), pch=1:2)
+
